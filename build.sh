ee2fe0ada4ed9d934f3527771772096fc407e13f
matt.turi <matt.turi@modalai.com>
2022-02-02 09:26:18 -0800

    template updates, gcc-7

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 ## first two for 820, last for 865
 TOOLCHAIN32="/opt/cross_toolchain/arm-gnueabi-4.9.toolchain.cmake"
 TOOLCHAIN64="/opt/cross_toolchain/aarch64-gnu-4.9.toolchain.cmake"
-TOOLCHAIN865="/opt/cross_toolchain/aarch64-gnu-8.toolchain.cmake"
+TOOLCHAIN865="/opt/cross_toolchain/aarch64-gnu-7.toolchain.cmake"
 
 # placeholder in case more cmake opts need to be added later
 EXTRA_OPTS=""
